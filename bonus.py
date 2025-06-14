88d7a5eccfdd9c461dd6bff9605e4e9926753c77
richylv <drafther@gmail.com>
2025-06-14 16:05:20 -0600

    Add read write files

--- bonus.py
+++ bonus.py
@@ -1,10 +1,9 @@
-filenames = ['1.raw data.txt','2.reports.txt','3.presentation.txt'] #lista
-filenames1 = ('1.raw data.txt','2.reports.txt','3.presentation.txt') #tuple no se puede modificar
-
+filenames = ['1.raw data.txt', '2.reports.txt', '3.presentation.txt']  #lista
+filenames1 = ('1.raw data.txt', '2.reports.txt', '3.presentation.txt')  #tuple no se puede modificar
 
 filename = '1.raw data.txt'
 print(filename[1])
-filename = filename.replace('.','-',1) # '1' cambia solo la primera vez que aparezca
+filename = filename.replace('.', '-', 1)  # '1' cambia solo la primera vez que aparezca
 print(filename)
 
 print(dir(str))
@@ -20,12 +19,21 @@ b = 10
 print(a * 10)
 print('hi' * 5)
 
-list = ['a','b','c']
+list = ['a', 'b', 'c']
 
-print(dir(list)) #muestra los metodos de list
+print(dir(list))  #muestra los metodos de list
 
 # list.__setitem__(1,'w') es lo mismo que list[1] = 'w'
 
-mylist = ['f','a','r']
-mylist.sort() #ordena ascendente
-mylist.sort(reverse=True) #orden desendente
+mylist = ['f', 'a', 'r']
+mylist.sort()  #ordena ascendente
+mylist.sort(reverse=True)  #orden desendente
+
+#file = open(r'C:\Users\Ricardo\Documents\python\toDo0', 'r')
+#r al inicio raw sting ya que puede haber caracteres especiales como \t
+#r como argumento para leer (read) el archivo; w para escribir (write)
+
+a = [1, 2, 3]
+b = [10, 20, 30]
+x = zip(a, b) # zip junta las dos listas [(1,10),(2,20),(3,30)]
+
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from traceback import print_exc
-
-todos = ['a','b','c']
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a to do: ")
+            todo = input("Enter a to do: ") + '\n'
+
+            file = open('todo.txt','r') #read file todo txt
+            todos = file.readlines() #asignar text from file to a list
+            file.close()
+
             todos.append(todo)
+
+            file = open('todo.txt', 'w') #write in a new file
+            file.writelines(todos)
+            file.close()
         case 'show' | 'display':
+            file = open('todo.txt','r')
+            todos = file.readlines()
+            file.close()
             for index, item in enumerate(todos):
                 print(f"{index + 1}-{item}")
         case 'edit':
