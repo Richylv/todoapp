83c5da7dc809dd2792d27821fc2e15f60340b7d1
richylv <drafther@gmail.com>
2025-06-19 14:51:09 -0600

    function2

--- bonus/functions.py
+++ bonus/functions.py
@@ -1,13 +1,26 @@
-def convert(feet_inches):
+def parse(feet_inches):
     print(feet_inches)
     parts = feet_inches.split(' ')
     print(parts)
     feet = float(parts[0])
     inches = float(parts[1])
 
+    return feet, inches
+
+def convert(feet, inches):
+    """ recive feet and inches and returns a message with the value in meters"""
     meters = feet * 0.3048 + inches * 0.0254
     return f'{feet} feet and {inches} inches is equal {meters} meters.'
 
+print(help(convert)) #print the description that is writing on the function
+
+
+feet_inches = input('Enter feet and inches: ') #expect input example 5 11
+f, i = parse(feet_inches)
+
+print(convert(f,i))
+
+#################################################3
 def get_average():
     with open('data.txt','r') as file:
         data = file.readlines()
@@ -23,6 +36,3 @@ def get_average():
 
 average = get_average()
 print(average)
-
-feet_inches = input('Enter feet and inches: ') #expect input example 5 11
-print(convert(feet_inches))
--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 
-def get_todos(file_path):
+def get_todos(file_path='todo.txt' ):
+    """ Read a text file and return the list of
+    to-do items """
     with open(file_path, 'r') as file:
         todos_local = file.readlines()
     return todos_local
 
 
-def write_todos(file_path,todos_arg):
+def write_todos(todos_arg,file_path='todo.txt'):
+    """  Write a text file with the list of
+    to-do items """
     with open(file_path, 'w') as file:
         file.writelines(todos_arg)
 
 while True:
-    filepath = 'todo.txt'
 
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     if user_action.startswith('add'):
         todo = user_action[4:] +'\n' # example user_action: 'add play games' extract content starting from index 4 'play games'
-        todos = get_todos(filepath)
+        todos = get_todos()
 
         todos.append(todo)
 
-        write_todos(filepath,todos)
+        write_todos(todos)
 
     elif 'show' in user_action:
-        todos = get_todos(filepath)
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')  # .strip('/n') remmove the caracter from the list
@@ -34,12 +37,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos(filepath)
+            todos = get_todos()
 
             new_todo = input('Enter new to do: ')
             todos[number] = new_todo + '\n'
 
-            write_todos(filepath,todos)
+            write_todos(todos)
 
         except ValueError:
             print('Your command is not valid')
@@ -48,13 +51,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos(filepath)
+            todos = get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(filepath,todos)
+            write_todos(todos)
 
             message = f'To Do {todo_to_remove} was removed from the list'
             print(message)
